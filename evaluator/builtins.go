e70683c70188edc339b4cba116cfb369bafb5a45
Anatolii Platonov <platonoff.dev@gmail.com>
2022-03-19 15:19:15 +0400

    Add first, last, rest builins

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -22,6 +22,78 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"first": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=%d", len(args), 1)
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return arg.Elements[0]
+				}
+
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"last": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=%d", len(args), 1)
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return arg.Elements[len(arg.Elements)-1]
+				}
+
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"rest": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=%d", len(args), 1)
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return &object.Array{Elements: arg.Elements[1:len(arg.Elements)]}
+				}
+
+				return NULL
+			default:
+				return newError("argument to `first` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"push": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=%d", len(args), 1)
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
+
+		},
+	},
 	"print": {
 		Function: func(args ...object.Object) object.Object {
 			for _, a := range args {
